c44c1ff5ece9b0dff682deda3b5911f11a12863e
AkiKurisu <2683987717@qq.com>
2024-05-08 16:33:48 +0800

    Update ReactExtensions.cs

--- Runtime/Legacy/React/ReactExtensions.cs
+++ Runtime/Legacy/React/ReactExtensions.cs
@@ -52,45 +52,45 @@ namespace Kurisu.Framework
             return trigger;
         }
         #region IObservable
-        public static IDisposable Subscribe(this IObservable<Action> akiEvent, Action action)
+        public static IDisposable Subscribe(this IObservable<Action> observable, Action action)
         {
-            akiEvent.Register(action);
-            return new CallBackDisposable(() => akiEvent.Unregister(action));
+            observable.Register(action);
+            return new CallBackDisposable(() => observable.Unregister(action));
         }
-        public static IDisposable Subscribe<T>(this IObservable<Action<T>> akiEvent, Action<T> action)
+        public static IDisposable Subscribe<T>(this IObservable<Action<T>> observable, Action<T> action)
         {
-            akiEvent.Register(action);
-            return new CallBackDisposable(() => akiEvent.Unregister(action));
+            observable.Register(action);
+            return new CallBackDisposable(() => observable.Unregister(action));
         }
-        public static IDisposable Subscribe<T, K>(this IObservable<Action<T, K>> akiEvent, Action<T, K> action)
+        public static IDisposable Subscribe<T, K>(this IObservable<Action<T, K>> observable, Action<T, K> action)
         {
-            akiEvent.Register(action);
-            return new CallBackDisposable(() => akiEvent.Unregister(action));
+            observable.Register(action);
+            return new CallBackDisposable(() => observable.Unregister(action));
         }
-        public static IDisposable Subscribe<T, K, F>(this IObservable<Action<T, K, F>> akiEvent, Action<T, K, F> action)
+        public static IDisposable Subscribe<T, K, F>(this IObservable<Action<T, K, F>> observable, Action<T, K, F> action)
         {
-            akiEvent.Register(action);
-            return new CallBackDisposable(() => akiEvent.Unregister(action));
+            observable.Register(action);
+            return new CallBackDisposable(() => observable.Unregister(action));
         }
-        public static void SubscribeOnce(this IObservable<Action> akiEvent, Action action)
+        public static void SubscribeOnce(this IObservable<Action> observable, Action action)
         {
-            action += () => akiEvent.Unregister(action);
-            akiEvent.Register(action);
+            action += () => observable.Unregister(action);
+            observable.Register(action);
         }
-        public static void SubscribeOnce<T>(this IObservable<Action<T>> akiEvent, Action<T> action)
+        public static void SubscribeOnce<T>(this IObservable<Action<T>> observable, Action<T> action)
         {
-            action += (a) => akiEvent.Unregister(action);
-            akiEvent.Register(action);
+            action += (a) => observable.Unregister(action);
+            observable.Register(action);
         }
-        public static void SubscribeOnce<T, K>(this IObservable<Action<T, K>> akiEvent, Action<T, K> action)
+        public static void SubscribeOnce<T, K>(this IObservable<Action<T, K>> observable, Action<T, K> action)
         {
-            action += (a, b) => akiEvent.Unregister(action);
-            akiEvent.Register(action);
+            action += (a, b) => observable.Unregister(action);
+            observable.Register(action);
         }
-        public static void SubscribeOnce<T, K, F>(this IObservable<Action<T, K, F>> akiEvent, Action<T, K, F> action)
+        public static void SubscribeOnce<T, K, F>(this IObservable<Action<T, K, F>> observable, Action<T, K, F> action)
         {
-            action += (a, b, c) => akiEvent.Unregister(action);
-            akiEvent.Register(action);
+            action += (a, b, c) => observable.Unregister(action);
+            observable.Register(action);
         }
         #endregion
         #region UnityEvents
