46bb596d28b55ed13023f27eed4c225e016c0bbc
AkiKurisu <2683987717@qq.com>
2024-10-27 16:26:59 +0800

    Add scheduler debugger editor window
    Remove scheduler runner editor

--- Runtime/Core/Schedulers/Interfaces/IScheduled.cs
+++ Runtime/Core/Schedulers/Interfaces/IScheduled.cs
@@ -6,25 +6,33 @@ namespace Kurisu.Framework.Schedulers
     /// </summary>
     internal interface IScheduled : IDisposable
     {
+        /// <summary>
+        /// Get handle of task
+        /// </summary>
+        /// <value></value>
         SchedulerHandle Handle { get; }
         /// <summary>
-        /// Get whether or not the scheduler has finished running for any reason.
+        /// Get whether or not the task has finished running for any reason.
         /// </summary>
         bool IsDone { get; }
         /// <summary>
-        /// Update scheduler
+        /// Whether the task is currently paused.
+        /// </summary>
+        bool IsPaused { get; }
+        /// <summary>
+        /// Update task
         /// </summary>
         void Update();
         /// <summary>
-        /// Stop a scheduler that is in-progress or paused. The scheduler's on completion callback will not be called.
+        /// Stop a task that is in-progress or paused. The task's on completion callback will not be called.
         /// </summary>
         void Cancel();
         /// <summary>
-        /// Pause a running scheduler. A paused scheduler can be resumed from the same point it was paused.
+        /// Pause a running scheduler. A paused task can be resumed from the same point it was paused.
         /// </summary>
         void Pause();
         /// <summary>
-        /// Continue a paused scheduler. Does nothing if the scheduler has not been paused.
+        /// Continue a paused task. Does nothing if the task has not been paused.
         /// </summary>
         void Resume();
     }
