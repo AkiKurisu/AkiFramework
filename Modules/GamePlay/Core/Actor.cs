d5476ee9002c28b58adc88eb788c76b71f91279d
AkiKurisu <2683987717@qq.com>
2025-01-02 23:15:30 +0800

    Use source generator to implement Actor flow interface

--- Modules/GamePlay/Core/Actor.cs
+++ Modules/GamePlay/Core/Actor.cs
@@ -1,29 +1,18 @@
-using System;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using Ceres.Annotations;
-using Ceres.Graph;
 using Ceres.Graph.Flow;
 using Ceres.Graph.Flow.Annotations;
 using Chris.Serialization;
 using UnityEngine;
 using UnityEngine.Assertions;
-using UObject = UnityEngine.Object;
 namespace Chris.Gameplay
 {
     /// <summary>
     /// Actor is a MonoBehaviour to place GameObject in GamePlay level.
     /// </summary>
-    public class Actor : MonoBehaviour, IFlowGraphContainer
+    [GenerateFlow]
+    public partial class Actor : MonoBehaviour, IFlowGraphContainer
     {
-        [NonSerialized]
-        private FlowGraph _graph;
-        
-        [SerializeField]
-        private FlowGraphData graphData;
-        
-        public UObject Object => this;
-        
         private GameWorld _world;
         
         private PlayerController _controller;
@@ -31,20 +20,6 @@ namespace Chris.Gameplay
         private ActorHandle _handle;
         
         private readonly HashSet<ActorComponent> _actorComponents = new();
-        
-        protected FlowGraph Graph
-        {
-            get
-            {
-                if (_graph == null)
-                {
-                    _graph = GetFlowGraph();
-                    _graph.Compile();
-                }
-
-                return _graph;
-            }
-        }
         
         [ImplementableEvent]
         protected virtual void Awake()
@@ -214,28 +189,6 @@ namespace Chris.Gameplay
             {
                 if (component is TComponent tComponent) components.Add(tComponent);
             }
-        }
-        
-        protected void ProcessEvent([CallerMemberName] string eventName = "", params object[] parameters)
-        {
-            using var evt = ExecuteFlowEvent.Create(eventName, parameters);
-            /* Execute event in quiet way */
-            Graph.TryExecuteEvent(this, evt.FunctionName, evt);
-        }
-        
-        public CeresGraph GetGraph()
-        {
-            return GetFlowGraph();
-        }
-
-        public FlowGraph GetFlowGraph()
-        {
-            return new FlowGraph(graphData.CloneT<FlowGraphData>());
-        }
-
-        public void SetGraphData(CeresGraphData graph)
-        {
-            graphData = (FlowGraphData)graph;
         }
     }
 }
